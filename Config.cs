a8cd834e9ffeb1bb55ce1249b11bbc6caef8c516
GoodPro712 <charliebrannan154@gmail.com>
2020-06-02 23:43:51 +1000

    config??

--- Config.cs
+++ Config.cs
@@ -1,50 +1,17 @@
-using System.IO;
-using Terraria;
-using Terraria.IO;
-using Terraria.ModLoader;
+/*using System.ComponentModel;
+using Terraria.ModLoader.Config;
 
 namespace OurStuffAddon
 {
-	public static class Config
+	//After converting this config to the new ModConfig system I have realised that it does nothing. lmao. - goodpro
+	[Label("Slime's Stuff Config")]
+	public class SlimesStuffConfig : ModConfig
 	{
-		public static bool HeartStoneNotices = true;
-		private static readonly string ConfigPath = Path.Combine(Main.SavePath, "Mod Configs", "OurStuffAddonConfig.json");
-		private static readonly Preferences Configuration = new Preferences(ConfigPath);
+		public override ConfigScope Mode => ConfigScope.ClientSide;
 
-		public static void Load()
-		{
-			bool success = ReadConfig();
-			
-			if (!success)
-			{
-				ErrorLogger.Log("Could not read OurStuffAddon's config file, creating new config file");
-				CreateConfig();
-			}
-		}
-
-		private static bool ReadConfig()
-		{
-			if (Configuration.Load())
-			{
-				Configuration.Get<bool>("Show 0.4 (HeartStone) Notices", ref Config.HeartStoneNotices);
-				if (HeartStoneNotices != true && HeartStoneNotices != false)
-				{
-					ErrorLogger.Log("'Show 0.4 (HeartStone) Notices' was a value other than true/false, setting to true");
-					HeartStoneNotices = true;
-				}
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-		}
-
-		private static void CreateConfig()
-		{
-			Configuration.Clear();
-			Configuration.Put("Show 0.4 (HeartStone) Notices", HeartStoneNotices);
-			Configuration.Save(true);
-		}
+		[Label("HeartStone Notices")]
+		[Tooltip("Toggle the showing of 0.4 (HeartStone) notices.")]
+		[DefaultValue(true)]
+		public bool HeartStoneNotices;
 	}
-}
+}*/
