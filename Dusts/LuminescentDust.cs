29a679145e548916ba147ff7e165fbc06f5357b6
GoodPro712 <charliebrannan154@gmail.com>
2020-06-03 00:00:58 +1000

    dust

--- Dusts/LuminescentDust.cs
+++ Dusts/LuminescentDust.cs
@@ -13,8 +13,8 @@ namespace OurStuffAddon.Dusts
 		public override void OnSpawn(Dust dust)
 		{
 			dust.alpha = 170; //this is the dust visibiliti, the bigger is the value less visible
-			dust.velocity *= 0.5f;  //this is the velocity of dust
-			dust.velocity.Y += 1f;  //and this is the velocity of dust when it goes up
+			dust.velocity *= 0.5f; //this is the velocity of dust
+			dust.velocity.Y += 1f; //and this is the velocity of dust when it goes up
 		}
 	}
 }
--- Dusts/SeafoamDust.cs
+++ Dusts/SeafoamDust.cs
@@ -13,8 +13,8 @@ namespace OurStuffAddon.Dusts
 		public override void OnSpawn(Dust dust)
 		{
 			dust.alpha = 1; //this is the dust visibiliti, the bigger is the value less visible
-			dust.velocity *= 1f;  //this is the velocity of dust
-			dust.velocity.Y += 1f;  //and this is the velocity of dust when it goes up
+			dust.velocity *= 1f; //this is the velocity of dust
+			dust.velocity.Y += 1f; //and this is the velocity of dust when it goes up
 		}
 	}
 }
