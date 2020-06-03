267ab3942f2a74387cfcdca6da0fbaf66aa2dabf
GoodPro712 <charliebrannan154@gmail.com>
2020-06-03 17:44:44 +1000

    Revert "CosmicWingsItem"
    This reverts commit fa8d24d70423316ac725a1cf773c8748ceed56dc.

--- Items/Accessories/CosmicWings.cs
+++ Items/Accessories/CosmicWings.cs
@@ -5,11 +5,11 @@ using Terraria.ID;
 namespace OurStuffAddon.Items.Accessories
 {
 	[AutoloadEquip(EquipType.Wings)]
-	public class CosmicWingsItem : ModItem
+	public class CosmicWings : ModItem
 	{
 		public override void SetStaticDefaults()
 		{
-			Tooltip.SetDefault("167 wing time, 2 accel");
+			Tooltip.SetDefault("(167 wing time, 2 accel");
 		}
 
 		public override void SetDefaults()
